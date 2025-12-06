2a70bd71726a4f666ff2aa006f3701139d645b87
csolanobusinesstd <csolano@businesstd.com>
2025-12-05 21:12:16 -0500

    Configurar base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/btd_web/', 
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
